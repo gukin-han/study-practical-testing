cbed3c2368dc4dde0d46cf32322a93fd2f0de9fd
Gukin Han <gukin.dev@gmail.com>
2024-02-14 08:37:34 +0900

    Presentation Layer 테스트 (1) end

--- cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.controller.product.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
@@ -10,11 +12,33 @@ import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * readOnly = true : 읽기전용
+ * CRUD 에서 CUD 동작 하지 않는다 / only read
+ * JPA: CUD 스냅샷 저장, 변경감지 x (성능 향상)
+ *
+ * CQRS - Command / Query 분리
+ * Master/Slave 구조에서 readOnly에 따라 DB 엔드포인트를 구분할 수 있다.
+ */
+@Transactional(readOnly = true) // 클래스 단위로 걸어준다.
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+
+    // 동시성 이슈
+    // UUID
+    @Transactional // cud를 메서드 단위에 필요에 따라 걸어준다.
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        final String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
+
 
     public List<ProductResponse> getSellingProducts() {
         final List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
@@ -22,5 +46,17 @@ public class ProductService {
         return products.stream()
                 .map(ProductResponse::of)
                 .collect(Collectors.toList());
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProduct();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        return String.format("%03d", nextProductNumberInt);
     }
 }
--- cafekiosk/src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -1,6 +1,7 @@
 package sample.cafekiosk.spring.domain.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +17,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllBySellingStatusIn(List<ProductSellingStatus> sellingStatuses);
 
     List<Product> findAllByProductNumberIn(List<String> productNumbers);
+
+    @Query(value = "select p.product_number from product p order by id desc limit 1", nativeQuery = true)
+    String findLatestProduct();
+    // 구현 방식에는 여러가지가 있지만, 그 방식과 별개로 동일한 테스트를 작성해서 테스트를 해야한다는 것을 강조
 }
